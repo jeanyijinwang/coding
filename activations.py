e1ccdd069ec93688df8e9e76534e5e8cbbb29225
NLPCamp <146028674+NLPCamp@users.noreply.github.com>
2023-09-26 21:58:24 +0000

    add homework for first week

---
+++ activations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# activation function and its derivative
+def tanh(x):
+    #TODO: return tanh
+
+
+def tanh_prime(x):
+    #TODO: return tanh derivative
+
---
+++ losses.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# loss function and its derivative
+def mse(y_true, y_pred):
+    #TODO: return mse
+
+def mse_prime(y_true, y_pred):
+    #TODO: return mse derivative
